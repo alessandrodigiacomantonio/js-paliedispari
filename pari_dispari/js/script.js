4ae7759890d9fc1070486b9a626be6263a9dc9a5
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2020-01-14 02:36:29 +0100

    Pari o Dispari con validatori

--- pari_dispari/js/script.js
+++ pari_dispari/js/script.js
@@ -2,12 +2,13 @@ do {
   var oddOrEven = prompt('Scegli tra \'pari\' o \'dispari\'').toLowerCase();
 } while (oddOrEven != 'pari' && oddOrEven != 'dispari');
 // console.log(oddOrEven);
-// do {
-  var number = parseInt(prompt('Scegli un numero pari o dispari, in base alla tua scelta precedente'))
-//   console.log(number)
-// } while ((oddOrEven == 'pari' && number % 2 != 0) || (oddOrEven == 'dispari' && number % 2 == 0));
-// console.log(oddOrEven,number);
-// while (number == isNaN && (oddOrEven == 'pari' && number % 2 != 0) && (oddOrEven == 'dispari' && number % 2 == 0));
+do {
+  var number = parseInt(prompt('Scegli un numero pari o dispari, in base alla tua scelta precedente'));
+} while (
+  (isNaN(number) || oddOrEven == 'pari' && number % 2 != 0)
+  ||
+  (isNaN(number) || oddOrEven == 'dispari' && number % 2 == 0)
+);
 var result = (Math.ceil(Math.random()*10))+number;
 console.log(result);
 if (result % 2 == 0 && oddOrEven == 'pari') alert('Hai vinto');
